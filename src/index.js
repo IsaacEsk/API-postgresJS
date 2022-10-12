55c3fa368b7fcd3af4e962e289d593ecde4e3505
Isaac Pineda <isaac.pineda@eskayser.com.mx>
2022-10-12 09:28:22 -0500

    Se agrego el coors para consulta de base de datos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+
 
 const app = express();
+
+
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // Routes
 app.use(require('./routes/index'));
